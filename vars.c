8af376896c840e6069bb44b3a93266bf9ba3dc8c
Theotislexis <51867982+Theotislexis@users.noreply.github.com>
2022-09-08 00:48:36 +0000

    Update vars.c

--- vars.c
+++ vars.c
@@ -38,7 +38,7 @@ int is_chain(info_t *info, char *buf, size_t *p)
 /**
  * check_chain - checks we should continue chaining based on last status
  * @info: the parameter struct
- * @buf: the char buffer
+ * @buf: the char buffer.
  * @p: address of current position in buf
  * @i: starting position in buf
  * @len: length of buf
